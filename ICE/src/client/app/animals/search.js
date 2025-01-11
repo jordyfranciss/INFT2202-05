a5f98025587f9889a0082c364f81ee0b7246b864
jordyfranciss <jordy.francis@dcmail.ca>
2025-01-10 22:57:14 -0500

    commit2

---
+++ ICE/src/client/app/animals/search.js
@@ -0,0 +1,7 @@
+/*
+    Name: Jordy Francis
+    filename: app.js
+    Course: INFT 2202
+    Date: January 10
+    Description: This is my general application script.  Functions that are required on every page should live here.
+*/
