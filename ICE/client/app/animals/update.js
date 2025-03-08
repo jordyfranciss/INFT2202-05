8cc4d5fbbe613229ec700f1e753d3465997fb272
jordyfranciss <jordy.francis@dcmail.ca>
2025-03-07 22:44:49 -0500

    COMMIT

--- ICE/client/app/animals/update.js
+++
@@ -1,7 +0,0 @@
-/*
-    Name:   Jordy Francis
-    filename: app.js
-    Course: INFT 2202
-    Date: January 10
-    Description: This is my general application script.  Functions that are required on every page should live here.
-*/
