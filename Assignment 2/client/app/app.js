ec41aad71935f56f766bf0757f5600debf11d73e
jordyfranciss <jordy.francis@dcmail.ca>
2025-03-10 11:56:25 -0400

    ASSIGNMENT 2
    Assignment 2

---
+++ Assignment 2/client/app/app.js
@@ -0,0 +1,6 @@
+// Name: Jordy Francis
+// Course: INFT 2202
+// Date: 2024-03-10
+
+// Set the current year in the footer
+document.getElementById('copyrightYear').textContent = new Date().getFullYear();
