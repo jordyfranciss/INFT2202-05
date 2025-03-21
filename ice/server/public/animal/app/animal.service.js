ea5992aaae6fced29708a759db918e8ed4fbfd8d
jordyfranciss <jordy.francis@dcmail.ca>
2025-03-21 18:25:11 -0400

    ICE 13
    ICE 13

--- ice/server/public/animal/app/animal.service.js
+++ ice/server/public/animal/app/animal.service.js
@@ -3,8 +3,9 @@
  *   we can export our service instance up here.
  */
 export default new AnimalService({
-    host: 'https://inft2202-server.onrender.com/',
-    //host: 'http://localhost:3091',
+    //host: 'https://inft2202-server.onrender.com/',
+    //host: 'http://localhost:3000',
+    host: window.location.origin,
     user: 'your student id'
 });
 
